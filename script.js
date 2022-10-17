f1430c77c009c4de37490592e92e7dde558ff75e
george827 <kinyanjuigeorge827@gmail.com>
2022-10-17 20:20:06 +0300

    update mobile menu

---
+++ script.js
@@ -0,0 +1,12 @@
+const humburger = document.querySelector(".my-menu");
+const navMenu = document.querySelector(".my-menu-nav");
+
+humburger.addEventListener("click", () => {
+    humburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    humburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
