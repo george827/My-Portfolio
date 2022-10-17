30eae7a300862449f70011c1ca128b3ddb777afa
george827 <kinyanjuigeorge827@gmail.com>
2022-10-17 21:04:59 +0300

    fix linters

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const humburger = document.querySelector(".my-menu");
-const navMenu = document.querySelector(".my-menu-nav");
+const humburger = document.querySelector('.my-menu');
+const navMenu = document.querySelector('.my-menu-nav');
 
-humburger.addEventListener("click", () => {
-    humburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+humburger.addEventListener('click', () => {
+  humburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    humburger.classList.remove("active");
-    navMenu.classList.remove("active");
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  humburger.classList.remove('active');
+  navMenu.classList.remove('active');
 }));
